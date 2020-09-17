f39b2e5d0a0775ee684ae73f7abd99bede2281f0
bashmentarium <mihigh.ciobanu@gmail.com>
2020-09-17 17:03:19 +0300

    Setup firebase and createUser cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
-const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const createUser = require('./create-user')
+const serviceAccount = require('./service.json')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://otpa-ebded.firebaseio.com',
+})
+
+exports.createUser = functions.https.onRequest(createUser)
