ccc22c25b980a50c765ea761bcbe3e63a0387080
bashmentarium <mihigh.ciobanu@gmail.com>
2020-09-17 20:08:20 +0300

    Write otp function

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const createUser = require('./create-user')
 const serviceAccount = require('./service.json')
+const requestOTP = require('./request_OTP')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,3 +13,4 @@ admin.initializeApp({
 })
 
 exports.createUser = functions.https.onRequest(createUser)
+exports.requestOneTimePassword = functions.https.onRequest(requestOTP)
