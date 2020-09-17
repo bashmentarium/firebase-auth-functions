1e487ea163abf321ed7f625077a940dc4585c799
bashmentarium <mihigh.ciobanu@gmail.com>
2020-09-17 20:49:54 +0300

    Add verify otp function

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions')
 const createUser = require('./create-user')
 const serviceAccount = require('./service.json')
 const requestOTP = require('./request_OTP')
+const verifyOTP = require('./verify_OTP')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,3 +15,4 @@ admin.initializeApp({
 
 exports.createUser = functions.https.onRequest(createUser)
 exports.requestOneTimePassword = functions.https.onRequest(requestOTP)
+exports.verifyOTP = functions.https.onRequest(verifyOTP)
